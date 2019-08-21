dd35f52637893930974bad7542f725659e19ca3b
rupar <rupar1999hl@gmail.com>
2019-08-21 10:18:51 +0630

    Revert "add cmt"
    This reverts commit 09ef0f0e52d52e492cac7f8a9c15b11c667f4f8b.

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $password='';
 $dbname='makeupdb';
 $tblname='makeuptbl';
 $host='localhost';
-#comment;
+
 $connecDB=mysqli_connect($host,$username,$password,$dbname)
 or die('could not connect to database');
 ?>
